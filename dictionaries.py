1604161deb0810f601ed796ebc9cc0b568f39f95
xzqt23 <angelo.agualada@gmail.com>
2023-02-16 18:14:01 +0800

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+# Dictionary items are ordered, changeable, and does not allow duplicates.
+person = {"name":"John","age":21,"address":"USA"}
+
+print(person["name"])
+person.keys()
+person.values()
+print(person)
